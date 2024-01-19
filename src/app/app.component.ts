a7f6e4e5c07c0720f5144d73c86a101c8dd9b173
Brunop27 <brunopinheiro715@gmail.com>
2024-01-19 13:36:26 -0300

    reorganizando pastas do projeto. Estilizado o componente article-titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { ArticleTitleComponent } from "./components/article-title/article-title.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, MenuBarComponent]
+    imports: [CommonModule, RouterOutlet, MenuBarComponent, ArticleTitleComponent]
 })
 export class AppComponent {
   title = 'blog';
