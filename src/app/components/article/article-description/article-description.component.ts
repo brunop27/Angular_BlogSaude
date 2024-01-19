82490652893d62e9deb1a9f6709225a07ec469d3
Brunop27 <brunopinheiro715@gmail.com>
2024-01-19 12:59:25 -0300

    adicionado pasta e estrutura dos components

---
+++ src/app/components/article/article-description/article-description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-description',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-description.component.html',
+  styleUrl: './article-description.component.css'
+})
+export class ArticleDescriptionComponent {
+
+}
---
+++ src/app/components/article/article-title/article-title.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-title',
+  standalone: true,
+  imports: [],
+  templateUrl: './article-title.component.html',
+  styleUrl: './article-title.component.css'
+})
+export class ArticleTitleComponent {
+
+}
